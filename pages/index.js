a89b653a97700df8f9d59df1b8efdb9877ef427e
e-zybkin <zybkin.evgeniy@gmail.com>
2023-03-18 17:11:57 +0300

    final build

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,95 @@
 import Api from "../scripts/components/Api.js";
 
-const myForm = document.getElementById('myForm');
-const input = myForm.shadowRoot.querySelector('.container__input');
-const dropdown = myForm.shadowRoot.querySelector('.container__dropdown');
+const myForm = document.querySelector('#myForm').shadowRoot;
+const input = myForm.querySelector('.container__input');
+const shortName = myForm.querySelector('#name_short');
+const fullName = myForm.querySelector('#name_full');
+const inn = myForm.querySelector('#inn_kpp');
+const address = myForm.querySelector('#address');
 
 const api = new Api({
   address: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party',
   token: 'f5dd144f0f9c2196d8b357ca872686b0c5ceb116'
 });
 
-input.addEventListener('input', () => {
+input.addEventListener('input', onInputChange);
+
+async function onInputChange() {
+  removeDropDown();
+
   const inputValue = input.value;
 
-  api.getAllMatch(inputValue)
+  if(inputValue.length === 0) {
+    return;
+  }
+
+  if(shortName.value.length > 0) {
+    clearFields();
+  }
+
+  const newArr = [];
+
+  await api.getAllMatch(inputValue)
   .then(result => {
-    dropdown.innerHTML = '';
-
     result.suggestions.forEach(item => {
-      const option = document.createElement('option');
-      option.text = item.value;
-      dropdown.appendChild(option)
-    });
-
-    dropdown.classList.add('container__dropdown_opened')
+      newArr.push(item)
+    })
   })
   .catch(error => {console.log(error)})
-})
+
+  createDropDown(newArr);
+}
+
+function createDropDown(arr) {
+  const listEl = document.createElement("ul");
+  listEl.className = "container__list";
+
+  arr.forEach(item => {
+    const listItem = document.createElement("li");
+    const listButton = document.createElement("button");
+    listButton.innerHTML = item.value;
+    listButton.value = item.value;
+    listButton.addEventListener('click', onDropDownButtonCLick);
+
+    listItem.appendChild(listButton);
+
+    listEl.appendChild(listItem);
+  });
+
+  myForm.querySelector('.container').appendChild(listEl);
+}
+
+function removeDropDown() {
+  const listEl = myForm.querySelector(".container__list");
+  if (listEl) {
+    listEl.remove();
+  };
+}
+
+function onDropDownButtonCLick(event) {
+  event.preventDefault();
+  const buttonEl = event.target;
+
+  api.getAllMatch(buttonEl.value)
+  .then(result => {
+    const data = result.suggestions[0].data;
+
+    shortName.value = data.name.short_with_opf;
+    fullName.value = data.name.full_with_opf;
+    inn.value = data.inn + ' / ' + data.kpp;
+    address.value = data.address.value;
+  })
+  .catch(error => {console.log(error)})
+
+  input.value = buttonEl.value;
+
+  removeDropDown();
+}
+
+function clearFields() {
+  shortName.value = '';
+  fullName.value = '';
+  inn.value = '';
+  address.value = '';
+  removeDropDown();
+}
